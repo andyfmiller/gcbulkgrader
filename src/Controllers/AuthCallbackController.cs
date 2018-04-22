fd9690358b756674b3ab798c66a9774a70d45e38
Andy Miller <andy@andyfmiller.com>
2018-04-21 20:55:34 -0700

    Wired up Google AuthCallbackController

---
+++ src/Controllers/AuthCallbackController.cs
@@ -0,0 +1,28 @@
+﻿using Google.Apis.Auth.OAuth2.AspMvcCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace gcbulkgrader.Controllers
+{
+    [Route("[controller]")]
+    public class AuthCallbackController : Google.Apis.Auth.OAuth2.AspMvcCore.Controllers.AuthCallbackController
+    {
+        public IConfiguration Configuration { get; set; }
+
+        public AuthCallbackController(IConfiguration config)
+        {
+            Configuration = config;
+        }
+
+        protected override FlowMetadata FlowData
+        {
+            get
+            {
+                var clientId = Configuration["Authentication:Google:ClientId"];
+                var clientSecret = Configuration["Authentication:Google:ClientSecret"];
+
+                return new AppFlowMetadata(clientId, clientSecret);
+            }
+        }
+    }
+}
