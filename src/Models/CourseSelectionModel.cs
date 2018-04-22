013b964974879efd45f2d4305fd027c34e8f77fb
Andy Miller <andy@andyfmiller.com>
2018-04-22 10:10:43 -0700

    Add Grader page with simple authentication.

---
+++ src/Models/CourseSelectionModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace gcbulkgrader.Models
+{
+    public class CourseSelectionModel
+    {
+        public SelectList Courses { get; set; }
+        public string PersonName { get; set; }
+        public string UserId { get; set; }
+    }
+}
